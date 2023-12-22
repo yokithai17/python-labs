ad596aec03ec76ab028583ebaea3c151ff7c53dc
yokithai <yokithai17@gmail.com>
2023-12-22 17:50:17 +0500

    123

--- task_1_1/part_1.py
+++ task_1_1/part_1.py
@@ -24,7 +24,7 @@ def uniform(size=1, precision=30) -> np.ndarray:
 
     binary_array = bits.reshape((amount_numbers, precision))
 
-    power_of_two = np.power((1/2), np.arange(1, precision + 1))
+    power_of_two = np.power((1 / 2), np.arange(1, precision + 1))
     numbers = np.dot(binary_array, power_of_two)
 
     return np.reshape(numbers, shape)
@@ -33,35 +33,35 @@ def uniform(size=1, precision=30) -> np.ndarray:
 def show():
     size = 200
     grid = np.linspace(-0.25, 1.25, 500)
-    sample = uniform(size, precision = 50)
+    sample = uniform(size, precision=50)
 
     # Отрисовка графика
-    plt.figure(figsize = (10, 4))
+    plt.figure(figsize=(10, 4))
 
     # отображаем значения случайных величин полупрозрачными точками
     plt.scatter(
         sample,
         np.zeros(size),
-        alpha = 0.4,
-        label = "Случайная величина"
+        alpha=0.4,
+        label="Случайная величина"
     )
 
     # по точкам строим нормированную полупрозрачную гистограмму
     plt.hist(
         sample,
-        bins = 10,
-        density = True,
-        alpha = 0.4,
-        color = "orange"
+        bins=10,
+        density=True,
+        alpha=0.4,
+        color="orange"
     )
 
     # рисуем график плотности
     plt.plot(
         grid,
         sps.uniform.pdf(grid),
-        color = 'red',
-        linewidth = 3,
-        label = "Плотность случайной величины"
+        color='red',
+        linewidth=3,
+        label="Плотность случайной величины"
     )
 
     plt.legend()
@@ -72,14 +72,14 @@ def show():
 def analyze():
     size = 100
 
-    plt.figure(figsize = (15, 3))
+    plt.figure(figsize=(15, 3))
 
     for i, precision in enumerate([1, 2, 3, 5, 10, 30]):
         plt.subplot(3, 2, i + 1)
         plt.scatter(
             uniform(size, precision),
             np.zeros(size),
-            alpha = 0.4
+            alpha=0.4
         )
         plt.yticks([])
         if i < 4: plt.xticks([])
